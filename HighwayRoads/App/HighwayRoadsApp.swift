7e0074a4531f28be024e738bf1692db215e04915
SBabchukTM <babchuk.serhii@trident-media.agency>
2026-01-05 14:55:25 +0200

    init

---
+++ HighwayRoads/App/HighwayRoadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighwayRoadsApp.swift
+//  HighwayRoads
+//
+//  Created by Serhii Babchuk on 05.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HighwayRoadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HRLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-HighwayRoads/Pods-HighwayRoads-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HighwayRoads : NSObject
+@end
+@implementation PodsDummy_Pods_HighwayRoads
+@end
